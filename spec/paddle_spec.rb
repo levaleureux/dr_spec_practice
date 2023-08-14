00d535db2f548617944d4619884e3606952df284
Valeureux <operateur1717@gmail.com>
2023-08-14 17:47:15 +0200

    create paddle spec

---
+++ spec/paddle_spec.rb
@@ -0,0 +1,11 @@
+spec "paddle" do
+
+  before do |args, assert|
+  end
+
+  it "start at vertical center"
+  it "can move up"
+  it "can move down"
+  it "have a max up"
+  it "have a max down"
+end
